ceba109c52ad3b877afd9f12590763e7c2afd00e
Adrian <adrian.navarro@students.fhu.edu>
2024-02-01 12:58:50 -0500

    Update main.py
    Used the help of the date time built in function.

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 #parse month should take in the text of the month and return the number 
 #as a string
 #January -> 1 (as a string)
+from datetime import datetime
+
 def parse_month(month):
-    pass
+    month_dict = {
+        'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06', 'July': '07', 
+        'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12' 
+        
+    }
+
+    if month in month_dict:
+        return month_dict[month]
+    else: 
+        return None
 
 #parse_date function should return the date formated as MM/DD/YYYY
 def parse_date(user_string):
-    pass
+    words = user_string.split()
+
+    if len(words) >= 3:
+        month = parse_month(words[0])
+        day = words[1].rstrip(',')
+        year = words[2]
+
+        if month and day.isdigit() and year.isdigit():
+            return f'{month}/{day}/{year}'
+    return None
 
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    pass
+    user_string = input()
+    formatted_date = parse_date(user_string)
+
+    print(formatted_date)
+    
